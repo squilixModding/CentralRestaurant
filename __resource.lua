f0bed6ad92fc36266cd05d68823b470dab5af070
nirual <143351491+squilixModding@users.noreply.github.com>
2023-08-28 23:47:28 +0200

    init

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version "77731fab-63ca-442c-a67b-abc70f28dfa5"
+
+use_fxv2_oal 'yes'
+lua54 'yes'
+game 'gta5'
+
+server_scripts {
+    "stream/graphics.lua"
+}
+
+shared_script '@es_extended/imports.lua'
+
+this_is_a_map 'yes'
